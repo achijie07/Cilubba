3a74717df1a6c3c894d73bf4181d3049050d4e3f
achijie07 <59381589+achijie07@users.noreply.github.com>
2024-08-25 06:17:48 +0800

    Update node.js

--- mho/node.js
+++ mho/node.js
@@ -14,7 +14,7 @@
             {
                     "drm_id": "first",
                     "referer": "https:\/\/www.visionplus.id",
-                    "logo_url": "https:\/\/static.mncnow.id\/images\/channel\/f0ec5a1a\/a884_r32.jpg",
+                    "logo_url": "https:\/\/s.id\/29Cyk",
                     "name": "INDOSIAR",
                     "stream_url": "https:\/\/atemecdnbalancer-voe.sysln.id\/live\/eds\/IndosiarHD\/mpd\/IndosiarHD.mpd",
                     "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
