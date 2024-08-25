7048181d700d3c4eb44fba2f62a7a23b4e6ad535
achijie07 <59381589+achijie07@users.noreply.github.com>
2024-08-25 20:21:00 +0800

    Update node.js

--- mho/node.js
+++ mho/node.js
@@ -3,14 +3,6 @@
             {
             "name": "National Tv",
             "channels": [
-             {
-                    "drm_id": "",
-                    "referer": "https:\/\/ben.quietlywheat23.shop",
-                    "logo_url": "https:\/\/s.id\/29CyY",
-                    "name": "EVENT",
-                    "stream_url": "https:\/\/fhlsport20.fls77.xyz\/token-J0J7meXGKvWG5Td7xPY6y2qClMCG1HjjOHSrwKlPEzD37rWfHUFdCI4ppBJMW0RJBCeIxjNmkg%3D%3Da\/cfall\/s2001\/v3b\/o882p1n4nUE0pQbiY3EmAmphZ3umqT0ho25y\/super\/3x_574181.m3u8",
-                    "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
-                },
                {
                     "drm_id": "moji",
                     "referer": "https:\/\/www.visionplus.id",
