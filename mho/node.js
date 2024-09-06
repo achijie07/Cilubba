968954c95be07215cf30b4660c209ec0477cb7b5
achijie07 <59381589+achijie07@users.noreply.github.com>
2024-09-06 23:25:01 +0800

    Update node.js

--- mho/node.js
+++ mho/node.js
@@ -1,10 +1,3 @@
-#EXTINF:-1 tvg-logo="https://play-gachor.my.id/logo/htv2/lokal/sctv.png" group-title="Lokal Auto", SCTV 
+#EXTINF:-1 tvg-logo="https://play-gachor.my.id/logo/htv2/lokal/sctv.png" group-title="SILAHKAN UPDATE VERSI", HUBUNGI ACHI UNTUK UPDATE
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Firefox/101.0 Safari/537.36 Edg/111.0.1661.43
 https://achijie07.github.io/Cilubba/siri_palaoka.m3u8
-
-
-#EXTINF:-1 tvg-logo="https://play-gachor.my.id/logo/htv2/lokal/indosiar.png" group-title="Lokal Auto", INDOSIAR 
-#https://bit.ly/3MeFJVc
-#KODIPROP:inputstream.adaptive.license_type=clearkey 
-#KODIPROP:inputstream.adaptive.license_key=5eaddccd893a47ac8b65f9b31963a3ae:881afa67ea57b41c1822d745d67559e6
-https://atemecdnbalancer-voe.sysln.id/live/eds/IndosiarHD/mpd/IndosiarHD.mpd
