83ce7a07a17a0b74d3e46f8b0949740bc9ead14c
achijie07 <59381589+achijie07@users.noreply.github.com>
2024-09-06 23:22:43 +0800

    Update node.js

--- mho/node.js
+++ mho/node.js
@@ -1,6 +1,6 @@
 #EXTINF:-1 tvg-logo="https://play-gachor.my.id/logo/htv2/lokal/sctv.png" group-title="Lokal Auto", SCTV 
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Firefox/101.0 Safari/537.36 Edg/111.0.1661.43
-https://streaming.indihometv.com/atm/DASH/sctv/manifest.mpd
+https://achijie07.github.io/Cilubba/siri_palaoka.m3u8
 
 
 #EXTINF:-1 tvg-logo="https://play-gachor.my.id/logo/htv2/lokal/indosiar.png" group-title="Lokal Auto", INDOSIAR 
